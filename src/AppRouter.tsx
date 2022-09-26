bab7664bdc85da94a1241019145512d6efddaf8f
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-26 22:40:08 +0300

    feat: created enum to store app routes

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,15 +2,16 @@ import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Main from './pages/Main'
 import PageLoadingIndicator from './pages/Configurator/components/UI/PageLoadingIndicator'
+import { AppRoutes } from './utils/routes'
 
 const LazyConfigurator = lazy(() => import('./pages/Configurator'))
 
 const AppRouter = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<Main />} />
+			<Route path={AppRoutes.MAIN} element={<Main />} />
 			<Route
-				path="/configurator"
+				path={AppRoutes.CONFIGURATOR}
 				element={
 					<Suspense fallback={<PageLoadingIndicator />}>
 						<LazyConfigurator />
---
+++ src/utils/routes.ts
@@ -0,0 +1,4 @@
+export enum AppRoutes {
+    MAIN = '/',
+    CONFIGURATOR = '/configurator'
+}
