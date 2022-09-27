09977a55401caf87d1e431ab3555fdb49c402ab0
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-27 20:09:41 +0300

    style: removed title from AccessoriesPopup

--- src/pages/Configurator/components/Sections/AccessoriesPopup/AccessoriesPopup.tsx
+++ src/pages/Configurator/components/Sections/AccessoriesPopup/AccessoriesPopup.tsx
@@ -1,46 +1,45 @@
-import { FC } from "react"
-import Portal from "../../../../../components/Portal"
+import { FC } from 'react'
+import Portal from '../../../../../components/Portal'
 import styles from './AccessoriesPopup.module.scss'
-import {ReactComponent as CloseCircle} from '../../../../../assets/icons/close-circle.svg'
-import {ReactComponent as Yacht} from '../../../../../assets/icons/yacht.svg'
-import AccessoriesList from "../../UI/AccessoriesList"
+import { ReactComponent as CloseCircle } from '../../../../../assets/icons/close-circle.svg'
+import { ReactComponent as Yacht } from '../../../../../assets/icons/yacht.svg'
+import AccessoriesList from '../../UI/AccessoriesList'
 
 interface AccessoriesPopupProps {
-    isOpened: boolean
-    onClose: () => void
+	isOpened: boolean
+	onClose: () => void
 }
 
-const AccessoriesPopup: FC<AccessoriesPopupProps> = ({isOpened, onClose}) => {
+const AccessoriesPopup: FC<AccessoriesPopupProps> = ({ isOpened, onClose }) => {
+	function handleStopPropagation(e: React.MouseEvent) {
+		e.stopPropagation()
+	}
 
-    function handleStopPropagation(e: React.MouseEvent) {
-        e.stopPropagation()
-    }
+	if (!isOpened) {
+		return null
+	}
 
-    if (!isOpened) {
-        return null
-    }
-
-    return (
-        <Portal onClose={onClose}>
-            <div className={styles.container} onClick={onClose} role='button' tabIndex={0}>
-                <div className={styles.content} onClick={handleStopPropagation}>
-                    <div className={styles.title}>
-                        <h3>Optional accessories</h3>
-                    </div>
-                    <div className={styles.body}>
-                        <div className={styles.header}>
-                            <div className={styles.logo}>
-                                <Yacht />
-                                <p>Kingdom Marine</p>
-                            </div>
-                            <CloseCircle onClick={onClose} className={styles['close-btn']}/>
-                        </div>
-                        <AccessoriesList />
-                    </div>
-                </div>
-            </div>
-        </Portal>
-    )
+	return (
+		<Portal onClose={onClose}>
+			<div
+				className={styles.container}
+				onClick={onClose}
+				role="button"
+				tabIndex={0}
+			>
+				<div className={styles.content} onClick={handleStopPropagation}>
+					<div className={styles.header}>
+						<div className={styles.logo}>
+							<Yacht />
+							<p>Kingdom Marine</p>
+						</div>
+						<CloseCircle onClick={onClose} className={styles['close-btn']} />
+					</div>
+					<AccessoriesList />
+				</div>
+			</div>
+		</Portal>
+	)
 }
 
 export default AccessoriesPopup
