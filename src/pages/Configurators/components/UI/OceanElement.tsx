cff3a9cee3743b682b52a02fd8bfe40993a0d133
Ilya Kruchinin <kr4chinin@mail.ru>
2022-10-01 07:05:25 +0300

    fix: better OceanElement typization

--- src/pages/Configurators/components/UI/OceanElement.tsx
+++ src/pages/Configurators/components/UI/OceanElement.tsx
@@ -1,13 +1,22 @@
-import { useRef, useMemo } from 'react'
+import { useRef, useMemo, RefObject } from 'react'
 import { extend, useThree, useLoader, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { Water } from 'three/examples/jsm/objects/Water.js'
+import { Object3DNode } from '@react-three/fiber'
 
 extend({ Water })
 
+declare global {
+	namespace JSX {
+		interface IntrinsicElements {
+			water: Object3DNode<Water, typeof Water>
+		}
+	}
+}
+
 const OceanElement = () => {
-	const ref = useRef()
-    const gl = useThree((state) => state.gl);
+	const ref = useRef<Water>()
+	const gl = useThree(state => state.gl)
 	const waterNormals = useLoader(
 		THREE.TextureLoader,
 		'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/waternormals.jpg'
@@ -25,20 +34,19 @@ const OceanElement = () => {
 			waterColor: 0x0064b5,
 			distortionScale: 40,
 			fog: false,
-            format: gl.outputEncoding
+			format: gl.outputEncoding
 		}),
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-		[waterNormals]
+		[waterNormals, gl.outputEncoding]
 	)
-	useFrame(
-		// @ts-ignore
-		(_, delta) => (ref.current.material.uniforms.time.value += delta / 5)
-	)
+	useFrame((_, delta) => {
+		if (ref && ref.current) {
+			ref.current.material.uniforms.time.value += delta / 5
+		}
+	})
 	return (
 		<>
-            {/* @ts-ignore */}
 			<water
-				ref={ref}
+				ref={ref as RefObject<Water>}
 				args={[geom, config]}
 				rotation-x={-Math.PI / 2}
 				position={[0, 0, 0]}
