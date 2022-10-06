7a202d68674a751d0094b69793d34fb016a45e8b
Ilya Kruchinin <kr4chinin@mail.ru>
2022-10-06 20:25:24 +0300

    feat: removed scrollbar from options dropdown

--- src/utils/materials/flybridge/materials[3].ts
+++ src/utils/materials/flybridge/materials[3].ts
@@ -1,5 +1,5 @@
 import { Children3 } from '../../urls/flybridge/Children[3]'
-import { TextureLoader, MeshBasicMaterial, Texture } from 'three'
+import { TextureLoader, MeshBasicMaterial } from 'three'
 import { setWrapAndRepeat } from '../../../helpers/setWrapAndRepeat'
 
 const initialTexture = new TextureLoader().load(Children3.variant4)
--- src/utils/materials/flybridge/materials[4].ts
+++ src/utils/materials/flybridge/materials[4].ts
@@ -2,35 +2,45 @@ import { Children4 } from '../../urls/flybridge/Children[4]'
 import { TextureLoader, MeshBasicMaterial, MeshMatcapMaterial } from 'three'
 
 const initialTexture = new TextureLoader().load(
-	'https://raw.githubusercontent.com/nidorx/matcaps/master/1024/1D2424_565F66_4E555A_646C6E.png'
+	'https://raw.githubusercontent.com/nidorx/matcaps/master/1024/3B3C3F_DAD9D5_929290_ABACA8.png'
 )
 export const initialMaterial4 = new MeshMatcapMaterial({
 	matcap: initialTexture
 })
 
 const wildGreenTexture = new TextureLoader().load(
-	'https://raw.githubusercontent.com/nidorx/matcaps/master/1024/181F1F_475057_616566_525C62.png'
+	'https://raw.githubusercontent.com/nidorx/matcaps/master/1024/5F5F5F_BDBDBD_A4A4A4_9C9C9C.png'
 )
 export const wildGreenMaterial4 = new MeshMatcapMaterial({
 	matcap: wildGreenTexture
 })
 
-const sunnyGreenTexture = new TextureLoader().load(Children4.variant2)
-export const sunnyGreenMaterial4 = new MeshBasicMaterial({
-	map: sunnyGreenTexture
+const sunnyGreenTexture = new TextureLoader().load(
+	'https://raw.githubusercontent.com/nidorx/matcaps/master/1024/070B0C_B2C7CE_728FA3_5B748B.png'
+)
+export const sunnyGreenMaterial4 = new MeshMatcapMaterial({
+	matcap: sunnyGreenTexture
 })
 
-const skyBlackTexture = new TextureLoader().load(Children4.variant3)
-export const skyBlackMaterial4 = new MeshBasicMaterial({ map: skyBlackTexture })
-
-const papyrusGreenTexture = new TextureLoader().load(Children4.variant4)
-export const papyrusGreenMaterial4 = new MeshBasicMaterial({
-	map: papyrusGreenTexture
+const skyBlackTexture = new TextureLoader().load(
+	'https://raw.githubusercontent.com/nidorx/matcaps/master/1024/2A6276_041218_739BA6_042941.png'
+)
+export const skyBlackMaterial4 = new MeshMatcapMaterial({
+	matcap: skyBlackTexture
 })
 
-const darkCloudsTexture = new TextureLoader().load(Children4.variant5)
-export const darkCloudsMaterial4 = new MeshBasicMaterial({
-	map: darkCloudsTexture
+const papyrusGreenTexture = new TextureLoader().load(
+	'https://raw.githubusercontent.com/nidorx/matcaps/master/1024/942967_D292B5_C76E9E_551A4C.png'
+)
+export const papyrusGreenMaterial4 = new MeshMatcapMaterial({
+	matcap: papyrusGreenTexture
+})
+
+const darkCloudsTexture = new TextureLoader().load(
+	'https://raw.githubusercontent.com/nidorx/matcaps/master/1024/2E763A_78A0B7_B3D1CF_14F209.png'
+)
+export const darkCloudsMaterial4 = new MeshMatcapMaterial({
+	matcap: darkCloudsTexture
 })
 
 const asphaltGrayTexture = new TextureLoader().load(Children4.variant6)
