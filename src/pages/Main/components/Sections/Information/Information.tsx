5abaa8fa436d3b4618ed6da4c097271f5d23f01e
Ilya Kruchinin <kr4chinin@mail.ru>
2022-11-18 20:29:06 +0300

    feat: add initials icon

--- src/pages/Main/components/Sections/Information/Information.tsx
+++ src/pages/Main/components/Sections/Information/Information.tsx
@@ -1,11 +1,14 @@
 import styles from './Information.module.scss'
+import { ReactComponent as InitialsIcon } from '../../../../../assets/icons/initials.svg'
 
 const Information = () => {
-  return (
-    <div className={styles.container}>
-      
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<div className={styles['initials-icon-wrapper']}>
+				<InitialsIcon />
+			</div>
+		</div>
+	)
 }
 
 export default Information
