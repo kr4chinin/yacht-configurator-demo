e555c0f91840104d3f9279251196eeda43e5da3c
Ilya Kruchinin <kr4chinin@mail.ru>
2022-11-19 19:23:22 +0300

    fix: change language selector design

--- src/pages/Main/components/UI/LanguageSwitch/LanguageSwitch.tsx
+++ src/pages/Main/components/UI/LanguageSwitch/LanguageSwitch.tsx
@@ -1,22 +1,16 @@
 import styles from './LanguageSwitch.module.scss'
-import { ReactComponent as RussianFlagIcon } from '../../../../../assets/icons/russian-language-flag.svg'
 import cn from 'classnames'
 
 const LanguageSwitch = () => {
 	return (
 		<div className={styles.container}>
+			<div className={cn(styles.option, { [styles.disabled]: true })}>RU</div>
 			<div
 				className={cn(styles.option, {
 					[styles.active]: true
 				})}
 			>
-				RU
-			</div>
-			<div className={styles.option}>EN</div>
-
-			<div className={styles.current}>
-				<RussianFlagIcon />
-				<span>RUS</span>
+				EN
 			</div>
 		</div>
 	)
