134d69da092218f3d972abfad3fd2ed63be38e7c
Ilya Kruchinin <kr4chinin@mail.ru>
2022-11-19 20:20:53 +0300

    feat: change info widget functionality

--- src/pages/Main/components/Sections/InfoWidgetList/InfoWidgetList.tsx
+++ src/pages/Main/components/Sections/InfoWidgetList/InfoWidgetList.tsx
@@ -1,12 +1,62 @@
+import { useState } from 'react'
 import InfoWidgetItem from '../../UI/InfoWidgetItem'
 import styles from './InfoWidgetList.module.scss'
 
+export interface InfoWidget {
+	date: string
+	text: string
+	id: number
+}
+
+const infoWidgetList: InfoWidget[] = [
+	{
+		id: 1,
+		date: '06/22',
+		text: 'Kingdom Marine was founded in Russia in June 2022 and has already managed to attract the attention of both domestic and foreign yacht manufacturers to its projects. Each yacht of Kingdom Marine is a sample of perfection of handcraftsmanship and thanks to unmatched attention to detail, advanced technology and excellent design the company can rightfully take its place as one of the leading Russian shipbuilders.'
+	},
+	{
+		id: 2,
+		date: '07/22',
+		text: 'Ipsum dolor sit amet consectetur adipisicing elit. Doloremque autem consectetur nam eum, eos debitis optio pariatur minus voluptatum architecto, officia, facilis veniam nihil perferendis temporibus. Ex deleniti doloremque modi voluptates odit placeat non perferendis ratione porro recusandae saepe, officiis eos quaerat esse libero. Quaerat illo corrupti quo maiores blanditiis?'
+	},
+	{
+		id: 3,
+		date: '08/22',
+		text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium ducimus aliquam, mollitia quaerat nihil nam excepturi ex temporibus deserunt. Quo excepturi molestiae delectus eum, maiores itaque ex veritatis eaque in sint rerum aperiam. Ipsum quis illo totam architecto dolorem soluta temporibus. Magni, consequuntur porro aperiam culpa nihil dolore. Nihil, facilis!'
+	}
+]
+
 const InfoWidgetList = () => {
-  return (
-    <div className={styles.container}>
-      <InfoWidgetItem />
-    </div>
-  )
+	const [activeItem, setActiveItem] = useState<InfoWidget>(infoWidgetList[0])
+
+	function handleNext() {
+		const nextItem = infoWidgetList.find(item => item.id === activeItem.id + 1)
+
+		if (nextItem) {
+			setActiveItem(nextItem)
+		}
+	}
+
+	function handlePrev() {
+		const prevItem = infoWidgetList.find(item => item.id === activeItem.id - 1)
+
+		if (prevItem) {
+			setActiveItem(prevItem)
+		}
+	}
+
+	return (
+		<div className={styles.container}>
+			<InfoWidgetItem
+				date={activeItem.date}
+				text={activeItem.text}
+				onNext={handleNext}
+				onPrev={handlePrev}
+        canClickNext={activeItem.id !== infoWidgetList.length}
+        canClickPrev={activeItem.id !== 1}
+			/>
+		</div>
+	)
 }
 
 export default InfoWidgetList
--- src/pages/Main/components/UI/InfoWidgetItem/InfoWidgetItem.tsx
+++ src/pages/Main/components/UI/InfoWidgetItem/InfoWidgetItem.tsx
@@ -1,22 +1,52 @@
 import styles from './InfoWidgetItem.module.scss'
+import { ReactComponent as ArrowIcon } from '../../../../../assets/icons/long-horizontal-arrow.svg'
+import { FC } from 'react'
+import { InfoWidget } from '../../Sections/InfoWidgetList/InfoWidgetList'
 
-const InfoWidgetItem = () => {
+interface InfoWidgetItemProps {
+	date: string
+	text: string
+	onNext: () => void
+	onPrev: () => void
+	canClickNext: boolean
+	canClickPrev: boolean
+}
+
+const InfoWidgetItem: FC<InfoWidgetItemProps> = ({
+	date,
+	text,
+	onNext,
+	onPrev,
+	canClickNext,
+	canClickPrev
+}) => {
+
+  console.log(canClickNext)
+
 	return (
 		<div className={styles.container}>
-			<div className={styles.date}>06/22</div>
+			<div className={styles.date}>{date}</div>
 			<div className={styles.content}>
-				<div className={styles.text}>
-					Kingdom Marine was founded in Russia in June 2022 and has already
-					managed to attract the attention of both domestic and foreign yacht
-					manufacturers to its projects. Each yacht of Kingdom Marine is a
-					sample of perfection of handcraftsmanship and thanks to unmatched
-					attention to detail, advanced technology and excellent design the
-					company can rightfully take its place as one of the leading Russian
-					shipbuilders.
-				</div>
+				<div className={styles.text}>{text}</div>
 				<div className={styles.controls}>
-
-        </div>
+					<div className={styles['next-btn-wrapper']}>
+						<button
+							className={styles['next-btn']}
+							onClick={onNext}
+							disabled={!canClickNext}
+						>
+							<span>Next</span>
+							<ArrowIcon />
+						</button>
+					</div>
+					<button
+						className={styles['prev-btn']}
+						onClick={onPrev}
+						disabled={!canClickPrev}
+					>
+						Prev
+					</button>
+				</div>
 			</div>
 		</div>
 	)
