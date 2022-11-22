1cf796fccdaef14a0af768f107928ff40a573222
Ilya Kruchinin <kr4chinin@mail.ru>
2022-11-22 21:50:29 +0300

    fix: remove footer in configurator

--- src/pages/Configurators/components/Sections/FlybridgeControlsWrapper.tsx
+++ src/pages/Configurators/components/Sections/FlybridgeControlsWrapper.tsx
@@ -2,7 +2,6 @@ import { FC, useState } from 'react'
 import { Group } from 'three'
 import ShowFullscreenButton from '../UI/ShowFullscreenButton'
 import FlybridgeControls from './FlybridgeControls'
-import Footer from './Footer'
 
 interface FlybridgeControlsWrapperProps {
 	model: Group
@@ -21,8 +20,6 @@ const FlybridgeControlsWrapper: FC<FlybridgeControlsWrapperProps> = ({
 				isFullscreenShown={isFullscreenShown}
 				toggleFullscreen={() => setIsFullscreenShown(prev => !prev)}
 			/>
-
-			<Footer isHidden={isFullscreenShown} />
 		</>
 	)
 }
