065f3a3ca6765c687d8486fe034c9a029df9b58f
Ilya Kruchinin <kr4chinin@mail.ru>
2022-11-26 22:24:39 +0300

    feat: OurMission section template

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,15 +1,16 @@
-import Navbar from '../Common/Navbar'
 import styles from './AboutUs.module.scss'
 import Introduction from './components/Sections/Introduction'
 import OurVision from './components/Sections/OurVision'
+import OurMission from './components/Sections/OurMission'
 
 const AboutUs = () => {
-  return (
-    <div className={styles.container}>
-      <Introduction />
-      <OurVision />
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<Introduction />
+			<OurVision />
+			<OurMission />
+		</div>
+	)
 }
 
 export default AboutUs
---
+++ src/pages/AboutUs/components/Sections/OurMission/OurMission.tsx
@@ -0,0 +1,14 @@
+import PrimaryTitle from '../../UI/PrimaryTitle'
+import styles from './OurMission.module.scss'
+
+const OurMission = () => {
+  return (
+    <div className={styles.container}>
+      <PrimaryTitle>
+        Our mission
+      </PrimaryTitle>
+    </div>
+  )
+}
+
+export default OurMission
