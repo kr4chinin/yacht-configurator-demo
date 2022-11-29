54d08ba7c89325824e2d03a53e0ba60c9a9a3fe3
Ilya Kruchinin <kr4chinin@mail.ru>
2022-11-29 21:42:32 +0300

    feat: finish JoinUs page

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -2,13 +2,15 @@ import styles from './AboutUs.module.scss'
 import Introduction from './components/Sections/Introduction'
 import OurVision from './components/Sections/OurVision'
 import OurMission from './components/Sections/OurMission'
+import JoinUs from './components/Sections/JoinUs'
 
 const AboutUs = () => {
 	return (
 		<div className={styles.container}>
-			<Introduction />
-			<OurVision />
-			<OurMission />
+			{/* <Introduction /> */}
+			{/* <OurVision /> */}
+			{/* <OurMission /> */}
+      <JoinUs />
 		</div>
 	)
 }
---
+++ src/pages/AboutUs/components/Sections/JoinUs/JoinUs.tsx
@@ -0,0 +1,39 @@
+import LoadableImage from '../../../../../components/LoadableImage/LoadableImage'
+import DelimiterIcon from '../../UI/DelimiterIcon'
+import styles from './JoinUs.module.scss'
+
+const JoinUs = () => {
+	return (
+		<div className={styles.container}>
+			<h4 className={styles.join}>JOIN US</h4>
+
+			<h2 className={styles.title}>
+				Become our Channel Partner and explore <br />
+				different opportunities
+			</h2>
+
+      <hr />
+
+			<div className={styles['options-block']}>
+				<div className={styles.option}>
+					<LoadableImage src="../../../../../../images/AboutUsPage/detailed-control.png" />
+					<p>DETAILED CONTROL</p>
+				</div>
+
+				<div className={styles.option}>
+					<LoadableImage src="../../../../../../images/AboutUsPage/become-an-investor.png" />
+					<p>BECOME AN INVESTOR</p>
+				</div>
+
+				<div className={styles.option}>
+					<LoadableImage src="../../../../../../images/AboutUsPage/good-benefit.png" />
+					<p>GOOD BENEFIT</p>
+				</div>
+			</div>
+
+			<DelimiterIcon />
+		</div>
+	)
+}
+
+export default JoinUs
