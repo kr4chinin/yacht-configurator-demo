a85f6d2ff250f61164b16bd47efed55b6d2ff68a
Ilya Kruchinin <kr4chinin@mail.ru>
2022-11-29 21:45:00 +0300

    feat: IdeaPage template

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -3,14 +3,16 @@ import Introduction from './components/Sections/Introduction'
 import OurVision from './components/Sections/OurVision'
 import OurMission from './components/Sections/OurMission'
 import JoinUs from './components/Sections/JoinUs'
+import Idea from './components/Sections/Idea'
 
 const AboutUs = () => {
 	return (
 		<div className={styles.container}>
-			{/* <Introduction /> */}
-			{/* <OurVision /> */}
-			{/* <OurMission /> */}
+			<Introduction />
+			<OurVision />
+			<OurMission />
       <JoinUs />
+      <Idea />
 		</div>
 	)
 }
---
+++ src/pages/AboutUs/components/Sections/Idea/Idea.tsx
@@ -0,0 +1,12 @@
+import DelimiterIcon from '../../UI/DelimiterIcon'
+import styles from './Idea.module.scss'
+
+const Idea = () => {
+  return (
+    <div className={styles.container}>
+      <DelimiterIcon />
+    </div>
+  )
+}
+
+export default Idea
